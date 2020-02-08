385064467fc9af99a3d2c31ccb14b027422722eb
Rob <robfarlow@gmail.com>
2020-02-08 12:45:22 -0500

    setup socket game sync

--- server/socket.js
+++ server/socket.js
@@ -8,37 +8,63 @@ const logError = debug('warhol:socket:error');
 
 function setupSocket(http) {
   const io = socketio(http);
-  io.on('connection', setup);
+  io.on('connection', async (socket) => {
+    try {
+      const socketId = socket.id;
+      const { hash, sessionId } = socket.handshake.query;
+      if (!hash || !sessionId) {
+        throw new Error();
+      }
+
+      const session = await db.Session.model.findOne({ sessionId }).populate('user');
+      if (!session || !session.user) {
+        throw new Error();
+      }
+
+      log(session.user._id, 'connected');
+
+      await db.User.model.findOneAndUpdate({ _id: session.user._id }, { socketId });
+
+      await socket.join(hash);
+
+      const game = await db.Game.model.findOneAndUpdate(
+        { hash },
+        {
+          $addToSet: {
+            users: session.user._id,
+          },
+        },
+        { new: true },
+      )
+        .select('users')
+        .populate('users');
+
+      await io.in(hash).emit('update-game', game);
+
+      socket.on('disconnect', async () => {
+        handleDisconnect(socket, hash, session.user._id, io);
+      });
+    } catch (err) {
+      logError(err);
+      socket.close();
+    }
+  });
 }
 
-async function setup(socket) {
-  try {
-    log('connection', socket.handshake);
-    const { hash, sessionId } = socket.handshake.query;
-    if (!hash || !sessionId) {
-      throw new Error();
-    }
-    // join room
-    socket.join(hash);
-
-    const session = await db.Session.model.findOne({ sessionId }).populate('user');
-    if (!session || !session.user) {
-      throw new Error();
-    }
-
-    await db.Game.model.findOneAndUpdate(
-      { hash },
-      {
-        $addToSet: {
-          users: session.user._id,
-        },
+async function handleDisconnect(socket, hash, userId, io) {
+  log(userId, 'disconnected');
+  const game = await db.Game.model.findOneAndUpdate(
+    { hash },
+    {
+      $pull: {
+        users: userId,
       },
-      { new: true },
-    ).populate('users');
-  } catch (err) {
-    logError(err);
-    socket.close();
-  }
+    },
+    { new: true },
+  )
+    .select('users')
+    .populate('users');
+  await io.in(hash).emit('update-game', game);
 }
 
 module.exports = setupSocket;
