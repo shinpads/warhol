4c016834961c531ee4097867176e69c2063adf58
Rob <robfarlow@gmail.com>
2020-05-08 19:17:52 -0400

    fix gcloud storage issue

--- server/drawingStore.js
+++ server/drawingStore.js
@@ -5,7 +5,8 @@ const { getFromCache, setInCache } = require('../util/redisClient');
 const gc = require('./lib/googleCloudStorage');
 const getObjectFromStream = require('./lib/getObjectFromStream');
 
-const log = debug('warhol:drawingStore');
+// const log = debug('warhol:drawingStore');
+const logError = debug('warhol:drawingStore:error');
 
 
 // put this in .env later, probably make a seperate bucket for testing
@@ -31,7 +32,7 @@ async function uploadDrawing(drawData, fileName) {
       .on('error', reject)
       .on('finish', resolve));
   } catch (err) {
-    log(err);
+    logError(err);
     throw new Error('Failed to upload to cloud', err);
   }
 }
@@ -47,6 +48,7 @@ async function downloadDrawing(fileName) {
     if (drawingFromCache) {
       return JSON.parse(drawingFromCache);
     }
+    if (process.env.NODE_ENV === 'development') return;
     const cloudFile = bucket.file(fileName);
     const exists = await cloudFile.exists();
     if (!exists[0]) throw new Error('File does not exist', fileName);
@@ -54,9 +56,10 @@ async function downloadDrawing(fileName) {
     if (!fileReadStream) throw new Error('Could not create readable stream from file', fileName);
     const drawing = await getObjectFromStream(fileReadStream)
       .catch(err => { throw new Error(err); });
-    await setInCache(fileName, drawing);
-    return JSON.parse(drawing);
+    await setInCache(fileName, JSON.stringify(drawing));
+    return drawing;
   } catch (err) {
+    logError(err);
     throw new Error('Failed to download file from cloud', err);
   }
 }
